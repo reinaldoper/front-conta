68ec2cd754245fb217dfa06873987f86582a87ba
reinaldoper <reinaldoper83@gmail.com>
2023-10-01 02:46:56 -0400

    refactor: ajusta navbar

--- src/components/NavbarUser.jsx
+++ src/components/NavbarUser.jsx
@@ -3,23 +3,24 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const NavbarUser = ({ data }) => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container style={{ backgroundColor: '#c4c776', borderRadius: '5px'}}>
-        <Navbar.Brand href="/">Login - Page</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Login - Page</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="clean">Delete</Nav.Link>
+            <Nav.Link as={Link} to="/clean">delete</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="create">Transaction-Create</NavDropdown.Item>
-              <NavDropdown.Item href="transaction">
+              <NavDropdown.Item as={Link} to="/create">Transaction-Create</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/transaction">
                 Transaction
               </NavDropdown.Item>
-              <NavDropdown.Item href="recover">RecoverAccount</NavDropdown.Item>
-              <NavDropdown.Item href="cashback">Cashback - Create</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/recover">RecoverAccount</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/cashback">Cashback - Create</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           { data ? <span style={{marginRight: '1.5vw'}}>cpf: { data.cpf }</span>: null}
